d1340073a6485eac205ff177200533a2646cec85
Pobuta Przemysław <Przemyslaw.Pobuta@asseco.pl>
2020-04-22 21:28:38 +0200

    feat: users improve

--- apps/app/src/app/utils/rest.service.ts
+++ apps/app/src/app/utils/rest.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injector } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface RestServiceInterface<T, ID> {
   getAll(): Observable<T[]>;
@@ -30,34 +31,46 @@ export class RestService<T, ID = number>
     item: Partial<T> | FormData,
     contentType = 'application/json'
   ): Observable<T> {
-    this.logRequest('post', item);
+    this.logRequest('post', { item });
     if (item instanceof FormData) {
-      return this._httpClient.post<T>(`${this.generateEndpointUrl()}`, item);
+      return this._httpClient
+        .post<T>(`${this.generateEndpointUrl()}`, item)
+        .pipe(tap(value => this.logResponse('post', value)));
     }
 
-    return this._httpClient.post<T>(`${this.generateEndpointUrl()}`, item, {
-      headers: new HttpHeaders().set('Content-Type', contentType)
-    });
+    return this._httpClient
+      .post<T>(`${this.generateEndpointUrl()}`, item, {
+        headers: new HttpHeaders().set('Content-Type', contentType)
+      })
+      .pipe(tap(value => this.logResponse('post', value)));
   }
 
   get(id: ID): Observable<T> {
-    this.logRequest('get', id);
-    return this._httpClient.get<T>(`${this.generateEndpointUrl()}/${id}`);
+    this.logRequest('get', { id });
+    return this._httpClient
+      .get<T>(`${this.generateEndpointUrl()}/${id}`)
+      .pipe(tap(value => this.logResponse('get', value)));
   }
 
   put(id: ID, item: Partial<T>): Observable<T> {
     this.logRequest('put', { id, item });
-    return this._httpClient.put<T>(`${this.generateEndpointUrl()}/${id}`, item);
+    return this._httpClient
+      .put<T>(`${this.generateEndpointUrl()}/${id}`, item)
+      .pipe(tap(value => this.logResponse('put', value)));
   }
 
   delete(id: ID): Observable<T> {
-    this.logRequest('delete', id);
-    return this._httpClient.delete<T>(`${this.generateEndpointUrl()}/${id}`);
+    this.logRequest('delete', { id });
+    return this._httpClient
+      .delete<T>(`${this.generateEndpointUrl()}/${id}`)
+      .pipe(tap(value => this.logResponse('delete', value)));
   }
 
   getAll(): Observable<T[]> {
     this.logRequest('getAll');
-    return this._httpClient.get<T[]>(`${this.generateEndpointUrl()}`);
+    return this._httpClient
+      .get<T[]>(`${this.generateEndpointUrl()}`)
+      .pipe(tap(value => this.logResponse('getAll', value)));
   }
 
   protected generateEndpointUrl(): string {
@@ -66,7 +79,14 @@ export class RestService<T, ID = number>
 
   protected logRequest(method?: string, message?: any): void {
     console.log(
-      `RestService [${this._serviceConfiguration.module}] ${method} `,
+      `[${this._serviceConfiguration.module}] RestService Request ${method}`,
+      message
+    );
+  }
+
+  protected logResponse(method?: string, message?: any): void {
+    console.log(
+      `[${this._serviceConfiguration.module}] RestService Response ${method}`,
       message
     );
   }
